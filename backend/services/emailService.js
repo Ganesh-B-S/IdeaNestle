aed511ed846f5e5fff844b77a82ba38da8c7701b
Ganesh158 <ganeshbs1210@gmail.com>
2026-05-30 02:02:32 +0530

    Fix SMTP transport

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -1,5 +1,6 @@
 const nodemailer = require("nodemailer");
-
+console.log("EMAIL_USER =", process.env.EMAIL_USER);
+console.log("EMAIL_PASS exists =", !!process.env.EMAIL_PASS);
 // Create transporter
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
